e7279d828573def2d7d6be61b20022276924619d
umer <mumer>
2020-10-28 11:33:35 +0500

    Component part 2

--- src/App.js
+++ src/App.js
@@ -2,18 +2,30 @@ import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap'
 import Menu from './components/MenuComponent'
 import './App.css';
+import { Component } from 'react';
+import { DISHES } from './shared/dishes'
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      dishes: DISHES
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,78 +1,65 @@
 import React, { Component } from 'react'
-import { Media } from 'reactstrap'
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap'
 
 class Menu extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            dishes: [
-                {
-                    id: 0,
-                    name: 'Uthappizza',
-                    image: 'assets/images/uthappizza.png',
-                    category: 'mains',
-                    label: 'Hot',
-                    price: '4.99',
-                    description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
-                },
-                {
-                    id: 1,
-                    name: 'Zucchipakoda',
-                    image: 'assets/images/zucchipakoda.png',
-                    category: 'appetizer',
-                    label: '',
-                    price: '1.99',
-                    description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
-                },
-                {
-                    id: 2,
-                    name: 'Vadonut',
-                    image: 'assets/images/vadonut.png',
-                    category: 'appetizer',
-                    label: 'New',
-                    price: '1.99',
-                    description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'
-                },
-                {
-                    id: 3,
-                    name: 'ElaiCheese Cake',
-                    image: 'assets/images/elaicheesecake.png',
-                    category: 'dessert',
-                    label: '',
-                    price: '2.99',
-                    description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
-                }
-            ],
+            selecDish: null
+        }
+
+    }
+
+
+    handleClick(item) {
+
+        this.setState({ selecDish: item })
+        console.log(item)
+    }
+
+    renderDish(item) {
+        if (item != null) {
+            return (
+                <Card>
+                    <CardImg width="100%" src={item.image} alt={item.name}></CardImg>
+                    <CardBody>
+                        <CardTitle>{item.name}</CardTitle>
+                        <CardText>{item.description}</CardText>
+                    </CardBody>
+                </Card>
+            )
+        } else {
+            return (
+                <div></div>
+            )
         }
     }
 
     render() {
 
-        const menu = this.state.dishes.map(item => {
+        const menu = this.props.dishes.map(item => {
 
             return (
-                <div key={item.id} className="col-12 mt-5">
-                    <Media tag="li">
-                        <Media left middle>
-                            <Media object src={item.image} alt={item.name} />
-                        </Media>
-                        <Media body className="ml-5">
-                            <Media heading>{item.name}</Media>
-                            <p>{item.description}</p>
-                        </Media>
-                    </Media>
-                </div>
+                <div key={item.id} className="col-12 col-md-5 m-1">
+                    <Card onClick={() => this.handleClick(item)}>
+                        <CardImg width="100%" src={item.image} alt={item.name}></CardImg>
+                        <CardImgOverlay >
+                            <CardTitle>{item.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
+                </div >
             )
         })
 
         return (
 
-            <div className="container">
+            <div className="container" >
                 <div className="row">
-                    <Media list>
-                        {menu}
-                    </Media>
+                    {menu}
+                </div>
+                <div className="row">
+                    {this.renderDish(this.state.selecDish)}
                 </div>
             </div>
         )
