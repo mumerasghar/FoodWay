f84a553f1f0aa3146e450bd8effdb765112ed8cb
umer <mumer>
2020-10-30 11:14:35 +0500

    SPA 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,6 +6,11 @@ import DishDetail from './DishDetailComponent'
 import Header from './HeaderComponent'
 import Footer from './FooterComponent'
 import { Switch, Route, Redirect } from 'react-router-dom'
+import Contact from './ContactComponent'
+import { COMMENTS } from '../shared/comments'
+import { LEADERS } from '../shared/leaders'
+import { PROMOTIONS } from '../shared/promotions'
+
 class Main extends Component {
 
     constructor(props) {
@@ -13,6 +18,9 @@ class Main extends Component {
 
         this.state = {
             dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
         }
     }
 
@@ -20,7 +28,9 @@ class Main extends Component {
 
         const HomePage = () => {
             return (
-                <Home />
+                <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter(leader => leader.featured)[0]} />
             )
         }
         return (
@@ -31,6 +41,7 @@ class Main extends Component {
                 <Switch>
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route exact path="/contactus" component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
                 {/* <Menu dishes={this.state.dishes}
@@ -39,7 +50,7 @@ class Main extends Component {
                 <Footer />
             </div>
         );
-    }  
+    }
 }
 
 export default Main;
